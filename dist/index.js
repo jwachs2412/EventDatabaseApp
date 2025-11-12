1554e6f624bbf6e53d1b662f196f7283fd46ef12
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-11-12 08:23:30 -0500

    added an Event interface, an array of Event objects and a an addEvent function that needs a little updating to make it more production ready

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const eventDatabase = [
+    { id: 1, type: "concert", name: "Chris Stapleton", date: "12/12/2018", row: 8, seat: 23, notes: "The concert was outstanding. 10/10" },
+    { id: 2, type: "sports", name: "Cleveland Browns v Pittsburgh Steelers", date: "11/6/1998", notes: "The Browns won 23-14 and played outstanding. 10/10" },
+    { id: 3, type: "concert", name: "Led Zepplin", date: "6/19/1974", row: "FF", seat: 2, notes: "The concert was outstanding. Led Zepplin blew the roof off! 10/10" },
+    { id: 4, type: "festival", name: "Bonnaroo", date: "7/12/2024 - 7/14/2024" }
+];
+function addEvent(obj) {
+    eventDatabase.push(obj);
+    console.log(eventDatabase);
+    return eventDatabase;
+}
+addEvent({
+    id: 5,
+    type: "theater",
+    name: "Hamilton",
+    date: "9/8/2023",
+    notes: "Incredible show, worth every penny!"
+});
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+interface Event {
+  id: number
+  type: string
+  name: string
+  date: string
+  row?: number | string
+  seat?: number | string
+  notes?: string
+}
+
+const eventDatabase: Event[] = [
+  { id: 1, type: "concert", name: "Chris Stapleton", date: "12/12/2018", row: 8, seat: 23, notes: "The concert was outstanding. 10/10" },
+  { id: 2, type: "sports", name: "Cleveland Browns v Pittsburgh Steelers", date: "11/6/1998", notes: "The Browns won 23-14 and played outstanding. 10/10" },
+  { id: 3, type: "concert", name: "Led Zepplin", date: "6/19/1974", row: "FF", seat: 2, notes: "The concert was outstanding. Led Zepplin blew the roof off! 10/10" },
+  { id: 4, type: "festival", name: "Bonnaroo", date: "7/12/2024 - 7/14/2024" }
+]
+
+function addEvent(obj: Event): Event[] {
+  eventDatabase.push(obj)
+
+  console.log(eventDatabase)
+  return eventDatabase
+}
+
+// AUTHOR'S SOLUTION - more production ready - understand this before implementing
+// function addEvent(obj: Omit<Event, "id">): Event[] {
+//   const newId = eventDatabase.length > 0 ? eventDatabase[eventDatabase.length - 1].id + 1 : 1
+//   eventDatabase.push({ id: newId, ...obj })
+//   console.log(eventDatabase)
+//   return eventDatabase
+// }
+
+addEvent({
+  id: 5,
+  type: "theater",
+  name: "Hamilton",
+  date: "9/8/2023",
+  notes: "Incredible show, worth every penny!"
+})
